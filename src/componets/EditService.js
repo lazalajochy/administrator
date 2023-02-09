f1a1daa50b03c4998a76bc1626e10c4b670c440d
lazalajochy <lazala1401jochy@outlook.com>
2023-02-09 09:09:50 -0400

    add service

--- src/componets/EditService.js
+++ src/componets/EditService.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import {useParams} from "react-router-dom"
+
 
 //        const res = await 
 
@@ -7,8 +7,7 @@ const EditService  = () => {
    
   
    
-    const {id} = useParams();
-    console.log(id)
+
 
 
    
